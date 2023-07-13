b849bd6f28bdd40579ed25f50e10165874688ce7
Vivek Sharma <2017ugcs071@nitjsr.ac.in>
2023-07-14 03:08:27 +0530

    visual search endpoint added

--- src/main/java/com/example/lookalikecelebrity/controller/CelebrityLookController.java
+++ src/main/java/com/example/lookalikecelebrity/controller/CelebrityLookController.java
@@ -2,43 +2,35 @@ package com.example.lookalikecelebrity.controller;
 
 import com.example.lookalikecelebrity.DTO.CelebrityDetailsDto;
 import com.example.lookalikecelebrity.service.CelebrityLookService;
+import com.example.lookalikecelebrity.service.VisualSearchApiIntegration;
+import com.meesho.storefront.clients.dtos.response.ProductFeedResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
 import java.util.List;
 
-@RestController
+@RestController("/api/v1/celeb-look-alike")
 public class CelebrityLookController {
 
     @Autowired
     CelebrityLookService celebrityLookService;
 
-    @PostMapping(value = "/api/v1/celeb-look-alike", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<List<CelebrityDetailsDto>> getCelebrityLookAlikeImages(@RequestParam("image1") MultipartFile multipartFile) throws Exception {
-        InputStream inputStream = multipartFile.getInputStream();
+    @Autowired
+    VisualSearchApiIntegration visualSearchApiIntegration;
 
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<List<CelebrityDetailsDto>> getCelebrityLookAlikeImages(@RequestParam("image1") MultipartFile multipartFile) throws Exception {
         return ResponseEntity.ok(celebrityLookService.getCelebrityLookAlikeImages(multipartFile));
     }
 
-
-//    @PostMapping("")
-//    public ResponseEntity<AzurePredictionResponse> getCelebrityPredictions(@RequestHeader("Prediction-Key") @NonNull String predictionKey,
-//                                                                           @Valid @RequestBody AzurePredictionRequest predictionRequest) {
-//
-//        try {
-//            return ResponseEntity.ok(celebrityLookService.getCelebrityLookAlikePredictions());
-//        } catch (Exception e){
-//            return ResponseEntity.status(500).build();
-//        }
-//
-//    }
-
+    @PostMapping("/products/{url}")
+    public ResponseEntity<ProductFeedResponse> getProducts(@PathVariable("url") String url) {
+        return ResponseEntity.ok(visualSearchApiIntegration.getVisualSearchResponse(url));
+    }
 }
