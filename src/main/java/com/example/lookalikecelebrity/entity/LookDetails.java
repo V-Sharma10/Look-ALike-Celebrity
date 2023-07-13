8d3fea2dbe5b37a4a507cd610f349c42e2fe47e5
Vivek Sharma <2017ugcs071@nitjsr.ac.in>
2023-07-14 02:34:39 +0530

    entity defined

--- src/main/java/com/example/lookalikecelebrity/entity/LookDetails.java
+++ src/main/java/com/example/lookalikecelebrity/entity/LookDetails.java
@@ -1,27 +1,38 @@
-//package com.example.lookalikecelebrity.entity;
-//
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.EqualsAndHashCode;
-//import lombok.NoArgsConstructor;
-//import lombok.experimental.SuperBuilder;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.Table;
-//
-//@Data
-//@SuperBuilder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@Entity
-//@Table(name = "look_details")
-//@EqualsAndHashCode(callSuper = true)
-//public class LookDetails extends BaseEntity {
-//
-//    @Column(name = "look_url_per_celeb_list")
-//    private S
-//
-//
-//}
+package com.example.lookalikecelebrity.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "look_details")
+@EqualsAndHashCode(callSuper = true)
+public class LookDetails extends BaseEntity {
+
+    @Column(name = "look_url_per_celeb_list")
+    private String lookUrlPerCelebList;
+
+    @Column(name="celeb_id")
+    private Long celebId;
+
+    @Column(name="look_id")
+    private Long lookId;
+
+    @Column(name = "look_url_per_celeb_hero_image")
+    private String lookUrlPerCelebHeroImage;
+
+    @Column(name = "look_name")
+    private String lookName;
+
+}
